94ebdd3cd98095ac878a0037f73dff1b5fcb7351
Sindhuja Parthasarathy <sarathy.sindhu@gmail.com>
2026-02-27 12:02:26 +0100

    Fixed the atachment file issue

--- controller.js
+++ controller.js
@@ -1,20 +1,21 @@
 import TodoItem from "./TodoItem.js";
 
+
 const todoForm = document.getElementById("todoForm");
 
+  const fileAttachmentsInput = document.getElementById("fileAttachments");
+ 
 todoForm.addEventListener("submit", function(event) {
   event.preventDefault();
 
   const formData=new FormData(event.target);
-  const fileDataInput= document.getElementById("fileAttachments");
-  const fileList = Array.from(fileDataInput.files);
-  
+
   let todoItem = new TodoItem(
     formData.get("title"),
     formData.get("description"),
     new Date(formData.get("dueDate")),
     formData.get("assignPerson"),
-    fileList
+    formData.getAll("attachements")
   );
 
   addTodoItemToUI(todoItem);
@@ -22,7 +23,17 @@ todoForm.addEventListener("submit", function(event) {
   console.log("Todo Item added:", todoItem);
 });
 
-
+fileAttachmentsInput.addEventListener("change", function(event) {
+    const files = event.target.files;
+    const filesList=document.getElementById("fileList");
+   
+    for(let i=0; i<files.length; i++){
+      const listItem=document.createElement("li");
+      listItem.textContent=files[i].name;
+      listItem.classList.add("list-group-item","border-0","bi","bi-paperclip");
+      filesList.appendChild(listItem);
+    }
+  });
 
 
 function addTodoItemToUI(todoItem){
@@ -38,15 +49,12 @@ function addTodoItemToUI(todoItem){
     else{
         todoItemTemplate.querySelector(".spanAssignedPerson").style.display = "none";
     }
-    
-    const attachmentsCount = todoItem.attachments.length;
-    if(attachmentsCount > 0){
-    todoItemTemplate.querySelector(".todoAttachement").textContent = `${attachmentsCount} attachment${attachmentsCount === 1 ? "" : "s"}`;
-    }
-    else{
-        todoItemTemplate.querySelector(".spanAttachment").style.display = "none";
-        
-    }
+      if (todoItem.attachments.length === 0 || todoItem.attachments.length === 1 && todoItem.attachments[0].size === 0) {  
+    todoItemTemplate.querySelector(".spanAttachment").style.display = "none";
+  } else {
+    todoItemTemplate.querySelector(".todoAttachement").textContent = todoItem.attachments.length + " attachment" + (todoItem.attachments.length > 1 ? "s" : "");
+  }
+
    
 //Deleting the todo item from the list when delete button is clicked
   const todoElement = todoItemTemplate.querySelector(".todoItemList");
